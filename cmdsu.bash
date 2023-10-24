6f1c69a8cd4f1d9793a9a29f6e40acc02f1fb3bd
OS Eight <147555122+OSEight@users.noreply.github.com>
2023-10-24 10:50:28 -0500

    SETUP

---
+++ cmdsu.bash
@@ -0,0 +1,8 @@
+$ nano ~/.bashrc 
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+alias myfms make me a sandwhich=' echo No! Do it yourself.;'
+alias myfms sudo make me a sandwhich=' echo Okay.;'
+alias myfms setup=' setup.bash;'
+alias myfms repo find=' echo Go to file explorer and find MyFMS/user/storage.;'
---
+++ setup.bash
@@ -0,0 +1,12 @@
+cd /
+mkdir MyFMS
+cd /MyFMS/
+mkdir vscode
+cd /MyFMS/vscode/
+sudo snap install --classic code
+sudo apt get update sudo apt install snapd
+cd /MyFMS
+mkdir user
+cd /MyFMS/user/
+mkdir storage
+cd /MyFMS/
